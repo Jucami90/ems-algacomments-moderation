06a9f54d32273e4a8e9ce984b9c5355f074be729
jucami90 <jucami90@gmail.com>
2025-11-23 23:13:27 -0500

    add project

---
+++ src/test/java/com/algaworks/algamoderation/moderation/ModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamoderation.moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
